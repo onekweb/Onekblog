fca46ed62c4c8c6abb7a9a1f974a745d0081e774
Mario Onekanda <mario.onkanda@hotmail.com>
2014-12-28 20:43:42 +0100

    login form

--- index.php
+++ index.php
@@ -7,17 +7,19 @@
 
     <body>
        <div id="loginCOntent">
-        <form action="index.php" method="POST" id="myForm">
-            <h1>Login</h1>
-           Username: <input type="text" name="username"><br/>
-           Password: <input type="password" name="password"><br/>
-            <input type="submit" value="Submit"><br/>   
-        </form>      
-       </div> 
-    <div id="footer">
-                    <p>&copy;2014 : Design by Onekweb : Mail: <a href="mailto:mario.onekanda@gmail.com"></a></p>
-                </div>
-            </div>
+            <form action="index.php" method="POST" id="myForm">
+                <h3>Sign in</h3>
+                    <hr/>
+                        <label for="username">Username</label><br/>
+                        <input type="text" name="username"    class="inputField"><br/>
+                        <label for="password">Password</label><br/>
+                        <input type="password" name="password" class="inputField"><br/><br/>   
+                        <input type="submit" value="Login" class="submit-text"><br/>                   
+                        <hr/ class="lineBttom">
+                         <!--   
+                        <label class="registerTxt1">New register?</label><label class="registerTxt2"> Register now</label>
+                        -->
+            </form>      
         </div>
     </body>
 </html>
